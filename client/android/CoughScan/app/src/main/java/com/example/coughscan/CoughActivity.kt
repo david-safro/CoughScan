e78e68f8bc041533547cbc7251db86dbf3704288
ianp <ian@porell.com>
2023-09-25 18:05:03 -0400

    add activity_cough.xml

---
+++ client/android/CoughScan/app/src/main/java/com/example/coughscan/CoughActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coughscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoughActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cough)
+    }
+}
--- client/android/CoughScan/app/src/main/java/com/example/coughscan/MainActivity.kt
+++ client/android/CoughScan/app/src/main/java/com/example/coughscan/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.coughscan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val coughNavButton = findViewById<Button>(R.id.button_nav_cough)
+        val symptomsNavButton = findViewById<Button>(R.id.button_nav_symptoms)
+
+        coughNavButton.setOnClickListener {
+            val intent = Intent(this, CoughActivity::class.java)
+            startActivity(intent)
+        };
     }
 }
