6fddfb7f4c8cd8fbf331aae4c4d871102f4562f1
David Safro <david.github24@gmail.com>
2023-10-01 09:40:28 -0400

    resizing

--- client/web/src/routes/+layout.svelte
+++ client/web/src/routes/+layout.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import Device from "svelte-device-info"
+    import Device from "svelte-device-info";
     import Footer from "../components/footer.svelte";
     import Navbar from "../components/navbar.svelte";
 
     onMount(() => {
-        (document.getElementById("fade-container") as HTMLDivElement).style.opacity = "1";
-        onresize = () => {
-            document.documentElement.style.fontSize = `${window.innerWidth / (325/2)}px`;
+        const fadeContainer = document.getElementById("fade-container") as HTMLDivElement;
+        const resizeHandler = () => {
+            fadeContainer.style.opacity = "1";
+            document.documentElement.style.fontSize = `${window.innerWidth / (325 / 2)}px`;
+        };
+        resizeHandler();
+        window.addEventListener('resize', resizeHandler);
+
+        if (Device.isMobile) {
+            alert("This website was not designed for mobile. Download the app for a better experience");
         }
-
-        if (Device.isMobile) alert("This website was not designed for mobile. Download the app for a better experience");
+        return () => {
+            window.removeEventListener('resize', resizeHandler);
+        };
     });
-
 </script>
 
 <Navbar />
