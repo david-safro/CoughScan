d8085f35b46cb9e4e0c1d51c7c7e77a16a7fe545
David Safro <david.github24@gmail.com>
2023-10-11 17:14:42 -0400

    better symptoms model (I need mental help)

---
+++ server/ai/symptoms/main.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import BorderlineSMOTE
+import pickle
+from model import Net
+df = pd.read_csv('data/Cleaned-Data.csv')
+df = df.drop(columns=['Country'])
+
+df['Severity'] = df['Severity_None'] * 0 + df['Severity_Mild'] * 1 + df['Severity_Moderate'] * 2 + df[
+    'Severity_Severe'] * 3
+df = df.drop(columns=['Severity_None', 'Severity_Mild', 'Severity_Moderate', 'Severity_Severe'])
+
+X = df.drop(columns=['Severity']).values
+y = df['Severity'].values
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+smote = BorderlineSMOTE(random_state=42)
+X, y = smote.fit_resample(X, y)
+
+X_train, X_val_test, y_train, y_val_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
+
+X_train_tensor = torch.FloatTensor(X_train)
+y_train_tensor = torch.LongTensor(y_train)
+X_val_tensor = torch.FloatTensor(X_val)
+y_val_tensor = torch.LongTensor(y_val)
+X_test_tensor = torch.FloatTensor(X_test)
+y_test_tensor = torch.LongTensor(y_test)
+
+with open('modelv1ADAM.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+model.eval()
+
+with torch.no_grad():
+    predictions = model(X_test_tensor)
+    _, predicted = torch.max(predictions, 1)
+    accuracy = accuracy_score(y_test_tensor, predicted)
+    report = classification_report(y_test_tensor, predicted)
+    conf_matrix = confusion_matrix(y_test_tensor, predicted)
+
+print(f"Accuracy: {accuracy}")
+print(f"Classification Report: \n{report}")
+print(f"Confusion Matrix: \n{conf_matrix}")
+
+
+def predict(model, input_data):
+
+    input_tensor = torch.FloatTensor([input_data])
+    model.eval()
+    with torch.no_grad():
+        output = model(input_tensor)
+        _, predicted_class = torch.max(output, 1)
+
+    return predicted_class.item()
+
+# sample data cuz too lazy to read from data
+input_data = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1]
+predicted_severity = predict(model, input_data)
+print(f"Predicted Severity: {predicted_severity}")
+
+
+
---
+++ server/ai/symptoms/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+class Net(nn.Module):
+    def __init__(self, X_train):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(X_train.shape[1], 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 128)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.dropout2 = nn.Dropout(0.4)
+        self.fc3 = nn.Linear(128, 64)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.dropout3 = nn.Dropout(0.3)
+        self.fc4 = nn.Linear(64, 4)
+
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.fc1(x)))
+        x = self.dropout1(x)
+        x = torch.relu(self.bn2(self.fc2(x)))
+        x = self.dropout2(x)
+        x = torch.relu(self.bn3(self.fc3(x)))
+        x = self.dropout3(x)
+        x = self.fc4(x)
+        return x
