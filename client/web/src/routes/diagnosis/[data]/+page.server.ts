7742da40d0fbe91f9c55ad08a59552baeab1d952
ianp <ian@porell.com>
2023-10-20 17:09:17 -0400

    YES!!!!!!!!!!

--- client/web/src/routes/diagnosis/[data]/+page.server.ts
+++ client/web/src/routes/diagnosis/[data]/+page.server.ts
@@ -5,33 +5,7 @@ import type { PageData } from "./$types.js";
 export async function load({ params }) {
     if (!params.data) throw error(404)
 
-    const test: DiagnosisInfo = {certainty:87.3,diagnosis:true,type:"cough"};
-
-    const parsedData = JSON.parse(decodeURIComponent(encodeURIComponent(JSON.stringify(test))));
-
-    const diagnosisInfo: DiagnosisInfo = {
-        diagnosis: parsedData.diagnosis,
-        certainty: parseFloat(parsedData.certainty),
-        type: parsedData.type == "cough" ? "cough" : "symptoms"
-    };
-
-    const coughTestOptions: CoughTestOptions = {
-        age: 16,
-        sex: "male",
-        respiratoryCondition: "",
-        feverMusclePain: true,
-        healthStatus: ""
-    };
-
-    const symptomInputOptions: SymptomInputOptions = {
-
-    }
-
-    const pageData: PageData = {
-        diagnosisInfo,
-        coughTestOptions,
-        symptomInputOptions
-    }
+    const pageData: PageData = JSON.parse(decodeURIComponent(params.data))
 
     return pageData;
 }
--- client/web/src/routes/input-symptoms/+page.svelte
+++ client/web/src/routes/input-symptoms/+page.svelte
@@ -1,10 +1,52 @@
 <script lang="ts">
+    import { redirect } from "@sveltejs/kit";
     import { symptomInputOptionLabels } from "../../lib/symptomInputOptions";
+    import type { PageData } from "./$types";
+
+    async function handleSubmit(event: SubmitEvent) {
+        event.preventDefault();
+
+        const formData = (event.target as HTMLFormElement).formData;
+
+        /*
+        try {
+            const response = await fetch('/your-server-endpoint', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log('Response from server:', responseData);
+            } else {
+                console.error('Server returned an error:', response.status);
+            }
+        } catch (error) {
+            console.error('Error while sending the data', error);
+        }
+        */
+
+        //temp
+        const responseData: PageData = {
+            diagnosisInfo: {
+                certainty: 84.7,
+                diagnosis: true
+            },
+            coughTestOptions: {
+                age: 14,
+                gender: "male",
+                respiratoryCondition: "yes",
+                feverMusclePain: false,
+                healthStatus: "healthy"
+            }
+        };
+        throw redirect(303, `/diagnosis/${encodeURIComponent(JSON.stringify(responseData))}`)
+    }
 </script>
 
 <link href="/css/input-symptoms.css" rel="stylesheet"/>
 <h1>Input Symptoms</h1>
-<form action="/" method="post">
+<form action="/" method="post" on:submit={handleSubmit}>
     {#each symptomInputOptionLabels as [symptomInputOptionLabel, propertyName]}
     <div>
         <label for={propertyName}>{symptomInputOptionLabel}</label>
